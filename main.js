54d3723abb38779d229df4451ad20d447f8caa0d
Mvpg <daniel.carlson.school@gmail.com>
2016-09-28 10:42:57 -0600

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-	$("#infoaboutme, #infoaboutme1, #infoaboutme2").hide();
-	
-	$("#aboutme").click(function(){
-		$("#infoaboutme, #infoaboutme1, #infoaboutme2").fadeToggle(500);
-	});
-});
