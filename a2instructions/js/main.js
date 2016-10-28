26a8808042723d2aaa0b90e445199425b7e62483
Mvpg <daniel.carlson.school@gmail.com>
2016-10-28 10:38:39 -0600

    Add files via upload

--- a2instructions/js/main.js
+++ a2instructions/js/main.js
@@ -8,14 +8,20 @@ $(document).ready(function(){
     			"font-size": "4em"
     		});
     	});
-    })
+    });
+
+	$("#Enlarge").click(function() {
+		alert("Test");
+		$(this).css("width:100%");
+	});
+
 
 	$("#hi").click(function () {
-		$("#hi").append('<img src="images/Finishedsnowboard.png" alt="hi">');
-		$("#hi").append('<img src="images/snowboard_sequence_5.png" alt="hi">');
-		$("#hi").append('<img src="images/snowboard_sequence_4.png" alt="hi">');
-		$("#hi").append('<img src="images/snowboard_sequence_3.png" alt="hi">');
-		$("#hi").append('<img src="images/snowboard_sequence_2.png" alt="hi">');
-		$("#hi").append('<div id="hi">');
+		$(this).append('<img src="images/Finishedsnowboard.png" alt="hi">');
+		$(this).append('<img src="images/snowboard_sequence_5.png" alt="hi">');
+		$(this).append('<img src="images/snowboard_sequence_4.png" alt="hi">');
+		$(this).append('<img src="images/snowboard_sequence_3.png" alt="hi">');
+		$(this).append('<img src="images/snowboard_sequence_2.png" alt="hi">');
+		$(this).append('<div id="hi">');
 	});
 });
